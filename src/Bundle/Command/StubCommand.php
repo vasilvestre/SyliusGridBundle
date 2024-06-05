71eb0af30f09d07cf809bd1f0aac4b2a0ed699e4
Loïc Frémont <lc.fremont@gmail.com>
2024-06-05 12:29:21 +0200

    Fix Stub command

--- src/Bundle/Command/StubCommand.php
+++ src/Bundle/Command/StubCommand.php
@@ -20,14 +20,14 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 abstract class StubCommand extends Command
 {
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         (new SymfonyStyle($input, $output))
             ->error(
-                \sprintf("To run \"%s\" you need the \"%s\" which is currently not installed.\n\nTry running \"composer require %s\".", static::$defaultName ?? '', 'MakerBundle', 'symfony/maker-bundle --dev'),
+                \sprintf("To run \"%s\" you need the \"%s\" which is currently not installed.\n\nTry running \"composer require %s\".", static::getDefaultName() ?? '', 'MakerBundle', 'symfony/maker-bundle --dev'),
             )
         ;
 
-        return 0;
+        return Command::SUCCESS;
     }
 }
--- src/Bundle/Command/StubMakeGrid.php
+++ src/Bundle/Command/StubMakeGrid.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\GridBundle\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'make:grid')]
 final class StubMakeGrid extends StubCommand
 {
-    protected static $defaultName = 'make:grid';
 }
